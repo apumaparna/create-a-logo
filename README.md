741c9cc994d2a344cd5af2d3a0e3f81767c2d9a2
Glitch (aparna-day-1) <none>
2020-07-30 01:53:05 +0000

    🐟🍢 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# create-a-logo
-CSSI day 1
+CSSI Day 1
+=================
+
+
+
+- README.md - This file!
+- index.html - Html page base structure, we include dependencies like JS files here
+- sketch.js - Your p5js code goes here.
+- assets/ - example logos
---
+++ sketch.js
@@ -0,0 +1,45 @@
+/* global createCanvas background ellipse 
+rect noFill stroke strokeWeight triangle fill*/
+
+function setup() {
+  // Code here runs only once
+  createCanvas(800, 600);
+}
+
+function draw() {
+  // Code here runs continuously
+  // background(255);
+  
+  noFill();
+  //strokeWeight(5); 
+  
+  //stroke(70, 136, 235);
+  // ellipse(50, 50, 50, 50);
+  
+  // stroke(250, 177, 50);
+  // ellipse(80, 70, 50, 50);
+  
+  // stroke(0, 0, 0);
+  // ellipse(110, 50, 50, 50);
+  
+  // stroke(64, 181, 38);
+  // ellipse(140, 70, 50, 50);
+  
+  // stroke(235,23,23);
+  // ellipse(170, 50, 50, 50);
+  // rect(80, 100, 20, 50); 
+  
+  fill(247, 20, 20)
+  stroke(247, 20, 20)
+  triangle(30,75,58,20,86,75)
+  
+  fill(232, 152, 46)
+  stroke(232, 152, 46)
+  triangle(30,75,58,20,58,55)
+  
+  fill(158, 28, 28)
+  stroke(158, 28, 28)
+  triangle(58,55,58,20,86,75) 
+  
+  
+}
